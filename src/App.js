8c633e4a85a952b74c69e12ec31e451b1f395b49
Corey Post <corey.post0120@gmail.com>
2020-10-07 02:52:59 -0600

    create few new files and folders and added api into api.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 //import React, { useState } from "react";
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Discover from "./pages/Discover";
-import About from "./pages/About";
-import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
---
+++ src/utils/API.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+// Export an object containing methods we'll use for accessing the random user generator API
+export default {
+    getEmployees: function() {
+        return axios.get("https://randomuser.me/api/?results=25&nat=us&origin=*");
+    },
+};
