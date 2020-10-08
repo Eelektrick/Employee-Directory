719dc3100f801ede0287813e5287c3f29f120eda
Corey Post <corey.post0120@gmail.com>
2020-10-08 01:30:48 -0600

    started pages index.js

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import API from "../utils/API";
+import Container from "../components/Container";
+import SearchForm from "../components/SearchForm";
+import SearchResults from "../components/SearchResults";
+import Row from "../components/Row";
+import Col from "../components/Col";
+
+class Index extends Component{
+
+}
+
+export default Index;
