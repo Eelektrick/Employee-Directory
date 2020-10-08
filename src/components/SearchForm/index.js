4dd96089bcde08312084ef7dca750bf24afcbdd6
Corey Post <corey.post0120@gmail.com>
2020-10-08 01:15:47 -0600

    cleaned code in searchform js

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -17,14 +17,6 @@ function SearchForm(props) {
           placeholder="Enter employee name here"
           id="employee"
         />
-        <datalist id="breeds">
-          {props.breeds.map(breed => (
-            <option value={breed} key={breed} />
-          ))}
-        </datalist>
-        <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
-          Search
-        </button>
       </div>
     </form>
   );
