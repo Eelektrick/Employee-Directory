08eadb505f25b258a8ce1d96cc7858d400d46909
Corey Post <corey.post0120@gmail.com>
2020-10-08 01:13:25 -0600

    created table in search results

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -1,15 +1,43 @@
 import React from "react";
 import "./style.css";
+import dateformat from "dateformat";
 
 function SearchResults(props) {
   return (
-    <ul className="list-group search-results">
-      {props.results.map(result => (
-        <li key={result} className="list-group-item">
-          <img alt="Dog" src={result} className="img-fluid" />
-        </li>
-      ))}
-    </ul>
+    <table className="table table-striped table table-striped table-dark">
+      <thead>
+        <tr>
+          <th scope="col">No.</th>
+          <th scope="col">Name</th>
+          <th scope="col">Image</th>
+          <th scope="col">DOB</th>
+          <th scope="col">Phone</th>
+          <th scope="col">Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.results.map((emp, index)=>(
+          <tr key={emp.dob.date}>
+            <th scope="row">{index+1}</th>
+            <td>
+              {emp.name.first} {emp.name.last}
+            </td>
+            <td>
+              <img src={emp.picture.medium} alt="employee img"></img>
+            </td>
+            <td>
+              {dateformat(emp.dob.date,"mm-dd-yyyy")}
+            </td>
+            <td>
+              {emp.phone}
+            </td>
+            <td>
+              {emp.email}
+            </td>
+          </tr>
+        ))};
+      </tbody>
+    </table>
   );
 }
 
