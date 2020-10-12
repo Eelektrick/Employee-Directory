d09932a8bbf4cbce2695582b54914a2875ca5d97
Corey Post <corey.post0120@gmail.com>
2020-10-11 21:34:40 -0600

    updated code to run react on github

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Index from "./pages/index";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
